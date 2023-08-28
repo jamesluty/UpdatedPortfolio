74c1aa4cfa4ad502fd2577daaddd46b122de61b8
James Luty <luty0125@gmail.com>
2023-08-28 15:53:59 -0700

    Initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../css/navbar.css';
+
+const Navbar = () => {
+    return (
+        <nav className="navbarWrapper">
+            <h1 className="navTitle">James Luty</h1>
+        </nav>
+    )
+}
+
+export default Navbar
